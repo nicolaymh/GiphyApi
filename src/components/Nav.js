f6e1fd922eba3b69bfe40a52831990d29ff45ba4
Nicolay Moreno <nicolay.mherrera@gmail.com>
2021-11-16 17:24:25 -0500

    Termiando nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -15,13 +15,23 @@ export const Nav = () => {
                     src='https://miro.medium.com/max/1400/1*tYt2BjbGzTo97IkADUNMGw.gif'
                     alt='img-portada'
                 />
-                <form className='form-nav' onSubmit={handleInputSearch}>
+                <form
+                    className='form-nav gradient-border'
+                    onSubmit={handleInputSearch}
+                >
                     <input
                         className='input-nav'
                         type='text'
                         onChange={handleChange}
                         value={inputSearch}
+                        placeholder='Search Gifs'
                     />
+                    <button className='nav-button' type='submit'>
+                        <img
+                            className='img-button'
+                            src='https://media2.giphy.com/media/0GsNMsRwDKKMjiwIe5/200w.webp?cid=ecf05e476ulngzl3gl6hehucmgcvdvdvsiiogvcuhzo63cu5&rid=200w.webp&ct=g'
+                        />
+                    </button>
                 </form>
             </div>
         </nav>
