534d9e4012cec06f6b7f0699e5b4e1b11e62e39d
Nicolay Moreno <nicolay.mherrera@gmail.com>
2021-11-15 22:30:43 -0500

    Comenzando a diseñar el nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,33 @@
 import React, { useContext } from 'react';
 import CallApiContext from '../context/CallApiContext';
 
+import '../style/Nav.css';
+
 export const Nav = () => {
     const { handleInputSearch, handleChange, inputSearch } =
         useContext(CallApiContext);
 
     return (
         <nav>
-            <form onSubmit={handleInputSearch}>
-                <input
-                    type='text'
-                    onChange={handleChange}
-                    value={inputSearch}
-                />
-            </form>
+            <div className='container container-nav'>
+                <div>
+                    <img src='' alt='img-portada' />
+                </div>
+                <div className='nav-contenedor-img'>
+                    <img
+                        className='img-avatar'
+                        src='https://media2.giphy.com/media/gVoBC0SuaHStq/giphy.gif?cid=ecf05e47q8zqohqieltne1rbsclysx1k7evjv3k237c99lfq&rid=giphy.gif&ct=g'
+                        alt='gif-nav'
+                    />
+                </div>
+                <form onSubmit={handleInputSearch}>
+                    <input
+                        type='text'
+                        onChange={handleChange}
+                        value={inputSearch}
+                    />
+                </form>
+            </div>
         </nav>
     );
 };
